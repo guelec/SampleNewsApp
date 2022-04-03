2f84d30eecf0391fc48b4948e72044b389bc6a66
Yiğit Güleç <yighitgulec@gmail.com>
2022-04-03 23:47:04 +0300

    Initial Commit

---
+++ SampleNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleNewsApp
+//
+//  Created by Yiğit Güleç on 3.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleNewsAppUITests/SampleNewsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SampleNewsAppUITestsLaunchTests.swift
+//  SampleNewsAppUITests
+//
+//  Created by Yiğit Güleç on 3.04.2022.
+//
+
+import XCTest
+
+class SampleNewsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
