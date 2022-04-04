68b4d593a0747d5311046a80182bb07042f351e0
Yiğit Güleç <yighitgulec@gmail.com>
2022-04-04 21:41:49 +0300

    String Extension for HTML

---
+++ SampleNewsApp/Extension/String+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  String+Ext.swift
+//  SampleNewsApp
+//
+//  Created by Yiğit Güleç on 4.04.2022.
+//
+
+import Foundation
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return nil
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
